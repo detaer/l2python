82622f519419b8c432c50affb08454f5e10bb1e9
detaer <detaer@blackbook.lan>
2018-11-21 18:32:42 -0800

    ohh yeahh

--- ga-python/mywork/movie_rating_lab/movie_app.py
+++ ga-python/mywork/movie_rating_lab/movie_app.py
@@ -1,3 +1,6 @@
 movie_title = "Back to the Future"
-movie_ratings = 8
+movie_ratings = "8"
 print(f"The movie {movie_title} is rated {movie_ratings}")
+sentence = ""
+sentence = movie_title + " is " + movie_ratings
+print(sentence)
