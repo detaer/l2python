a055bdcfdcb3984610a766fd2b93e3cced021561
detaer <detaer@blackbook.lan>
2018-11-21 18:28:23 -0800

    lessons

--- ga-python/mywork/lesson2.py
+++ ga-python/mywork/lesson2.py
@@ -9,10 +9,37 @@
 # print(f"How many tacos did you eat? {tacos_i_have_eaten} damn fatty")
 
 #make a variable about how many guitars I own
-guitars_i_own = 2
-guitars_nikhil_owns = 0
+# guitars_i_own = 2
+# guitars_nikhil_owns = 0
 #give some guitars to nikhil
-guitars_i_own = guitars_i_own - 2
-guitars_nikhil_owns = guitars_nikhil_owns + 2
-print(f"I have {guitars_i_own} guitars.")
-print(f"Nikhil has {guitars_nikhil_owns} guitars!")
+# guitars_i_own = guitars_i_own - 2
+# guitars_nikhil_owns = guitars_nikhil_owns + 2
+# print(f"I have {guitars_i_own} guitars.")
+# print(f"Nikhil has {guitars_nikhil_owns} guitars!")
+
+# guitars_i_own += 3
+# print(guitars_i_own)
+
+# Declare two variables `num1` and `num2` and assign them to any numbers you'd like.
+# num1, num2 = 1,2
+# Set `num1` to the result of subtracting `num1` from the `num2`.
+# num1 = (num1 - num2)
+# Create a new variable `num3` that will help us tell if `num2` is even or odd.
+# num3 = num2 % 2
+# Using shorthand, add 5 to `num1`.
+# num1 += 5
+# Print out `num1`, `num2`, and `num3`
+# print(num1, num2, num3)
+
+# name = "Morty"
+# car = "Delorean"
+# speed = "88"
+# sentence = ""
+
+# print(name, car, speed)
+# speed = speed + "4"
+# print(speed)
+
+# sentence = name + " " + "is driving his" + " " + car + " " + speed
+# print(sentence)
+
---
+++ ga-python/mywork/movie_rating_lab/movie_app.py
@@ -0,0 +1,3 @@
+movie_title = "Back to the Future"
+movie_ratings = 8
+print(f"The movie {movie_title} is rated {movie_ratings}")
