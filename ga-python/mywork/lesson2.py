ffcd0ec94fd90fcb4d9560c766256b4b47200a98
detaer <detaer@blackbook.lan>
2018-11-21 17:46:08 -0800

    wooo

---
+++ ga-python/mywork/lesson2.py
@@ -0,0 +1,18 @@
+# print(5**10)
+
+# tacos_i_have_eaten = 0
+
+# tacos_i_have_eaten = tacos_i_have_eaten + 1
+# tacos_i_have_eaten = tacos_i_have_eaten + 1
+# tacos_i_have_eaten = tacos_i_have_eaten + 1
+# tacos_i_have_eaten = tacos_i_have_eaten + 1
+# print(f"How many tacos did you eat? {tacos_i_have_eaten} damn fatty")
+
+#make a variable about how many guitars I own
+guitars_i_own = 2
+guitars_nikhil_owns = 0
+#give some guitars to nikhil
+guitars_i_own = guitars_i_own - 2
+guitars_nikhil_owns = guitars_nikhil_owns + 2
+print(f"I have {guitars_i_own} guitars.")
+print(f"Nikhil has {guitars_nikhil_owns} guitars!")
