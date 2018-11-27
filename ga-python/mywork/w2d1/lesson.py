cf9b61ac617173663a1beec6db8bbbcdc1637c0b
detaer <detaer@blackbook.lan>
2018-11-26 18:45:13 -0800

    workings

--- ga-python/mywork/w2d1/lesson.py
+++ ga-python/mywork/w2d1/lesson.py
@@ -7,3 +7,77 @@
 # print(50 > 100)
 # print("d" < "a")
 
+# print(5 = "5")
+
+# print("5 == 5 is..", 5 == 5)
+
+# print("6 == 3 is...", 6 == 3)
+
+# print("'5' == 5 is..", "5" == 5)
+
+# print("8 > 8", 8 > 8)
+
+# print("8 >= 8", 8 >= 8)
+
+# print("8 <= 15", 8 <= 15)
+
+# print('7 != "7"', 7 != "7")
+
+# print("6 == 7", 6 == 7)
+
+# print("6 != 7", 6 != 7)
+
+# You Do: Make an elif statement to determine what "Generation" you are:
+# my_age = 20
+
+# Gen Z are under 20
+# if my_age < 20:
+    # print("genz")
+# Millenials are 20 to 35
+# elif my_age >= 20 and my_age <= 35:
+  #   print("Millenials")
+# Gen X is 36 to 50
+# elif my_age >= 36 and my_age <= 50:
+  #   print("genx")
+#  Baby Boomers are 51 - 75
+# elif my_age >= 51 and my_age <= 75:
+  #   print("boomers")
+#  Greatest Generation is 76+
+# else:
+  #   print("old")
+
+# number = 11
+# remainder = number % 2
+# For an even number, print "It's even!"
+# print(remainder)
+# if remainder == 0:
+    # print("Even!")
+# For an odd number, print "It's odd!"
+# elif remainder != 0:
+  #   print("Odd!")
+
+# my_class = ["Brandi", "Zoe", "Steve", "Aleksander", "Dasha"]
+# print(my_class[0], my_class[2])
+
+# names = ["Holly", "Juan", "Ming"]
+# print(names[0])
+
+# numbers = [2, 4, 6, 8]
+# print(numbers[0])
+
+# my_class = ["Brandi", "Zoe", "Steve", "Aleksander", "Dasha"]
+# my_class.append("Sonyl")
+# print(len(my_class))
+
+# Save a list with the numbers 2, 4, 6, and 8 into a variable called numbers.
+# numbers = [2, 4, 6, 8]
+# Print the max of numbers.
+# print(max(numbers))
+# Append 3 to numbers.
+# numbers.append(3)
+# Print out the average number (divide the sum of numbers by the length).
+# Numbers_Count = sum(numbers)
+# Numbers_Length = len(numbers)
+# print(Numbers_Count / Numbers_Length )
+# Print numbers.
+# print(numbers)
