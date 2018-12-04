b1f42922193675d76b53f5ab6604a9a46540b183
detaer <detaer@blackbook.lan>
2018-12-03 18:09:12 -0800

    adding stuff

---
+++ ga-python/mywork/w3d1/lesson_1.py
@@ -0,0 +1,52 @@
+# seasons = ('spring', 'summer', 'autumn', 'winter')
+# print(seasons)
+
+# seasons.append('dogs')
+
+# my_set = {'green', 'red', 'tacos'}
+# for each in my_set:
+#     print(each)
+
+# set1 = {1,2,3,4,5}
+# set2 = {5,6,7,8,9,0}
+
+# print(set1.intersection(set2))
+
+# print(set1.difference(set2))
+
+### Creation ###
+# List
+# my_list = ["red",  "yellow", "green", "red"]
+# Sets
+# my_set = {"red",  "yellow", "green"}
+# my_set2 = set(my_list)
+# my_set = set(a_list_to_convert)
+
+### Appending a New Value ###
+# my_list.append("blue")
+# my_set.add("blue")
+
+### Appending a Duplicate ###
+# my_list.append("blue")
+# => my_list = ["red",  "yellow", "green", "red", "blue",  "blue"]
+# my_set.add("blue")
+# => my_set = {"red",  "yellow", "green", "blue"}
+
+### Removing items: ###
+# my_list.pop(1)
+# my_set.remove("red")
+
+# unique_colors = set(["red", "yellow", "green", "red"])
+# print("unique_colors is", type(unique_colors))
+# --
+# unique_colors_2 = ["red", "yellow", "green", "red"]
+# print("unique_colors_2 is", type(unique_colors_2))
+# --
+# unique_colors_3 = ("red", "yellow", "green", "red")
+# print("unique_colors_3 is", type(unique_colors_3))
+# --
+# my_number = 2
+print("my_number is", type(my_number))
+# --
+my_string = "Hello!"
+print("my_string is", type(my_string))
