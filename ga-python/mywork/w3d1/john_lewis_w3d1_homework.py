fa3b5ff5a230c4779d34fc93aa27fcdfa71443a4
detaer <detaer@blackbook.lan>
2018-12-04 18:03:02 -0800

    updated a character and the output

--- ga-python/mywork/w3d1/john_lewis_w3d1_homework.py
+++ ga-python/mywork/w3d1/john_lewis_w3d1_homework.py
@@ -1,7 +1,7 @@
 # My favorite romance movies
 # title, release year, runtime, tagline, main characters
 romantic_movie1 = ("The Princess Bride", 1987, 98, "The story of a man and a woman who lived happily ever after.", ["Buttercup", "Westley", "Fezzik", "Inigo Montoya", "Vizzini"])
-romantic_movie2 = ("Groundhog Day", 1993, 101, "He's having the day of his life… over and over again.", ["Phil Connors"])
+romantic_movie2 = ("Groundhog Day", 1993, 101, "He's having the day of his life... over and over again.", ["Phil Connors"])
 romantic_movie3 = ("Amélie", 2001, 122, "One person can change your life forever.", ["Amélie Poulain", "Nino Quincampoix", "The Garden Gnome"])
 romantic_movies = (romantic_movie1, romantic_movie2, romantic_movie3)
 # expected output
@@ -13,10 +13,10 @@ romantic_movies = (romantic_movie1, romantic_movie2, romantic_movie3)
 print("Here are my favorite romance movies:")
 
 for each in romantic_movies:
-    print(f"{each[0]} {each[1]}: {each[3]}")
+    print(f"{each[0]} ({each[1]}): {each[3]}")
 
 #actual output is commented here
 # Here are my favorite romance movies:
-# The Princess Bride 1987: The story of a man and a woman who lived happily ever after.
-# Groundhog Day 1993: He's having the day of his life… over and over again.
-# Amélie 2001: One person can change your life forever.
+# The Princess Bride (1987): The story of a man and a woman who lived happily ever after.
+# Groundhog Day (1993): He's having the day of his life... over and over again.
+# Amélie (2001): One person can change your life forever.
