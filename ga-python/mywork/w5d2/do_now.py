b3ac16f2e06ecfb65da4b5da479d6539a7e491b1
detaer <detaer@gmail.com>
2018-12-19 17:48:56 -0800

    another class, I am so classy

--- ga-python/mywork/w5d2/do_now.py
+++ ga-python/mywork/w5d2/do_now.py
@@ -11,3 +11,33 @@ newest_list = [word + ' pow' for word in old_list]
 
 print(new_list)
 print(newest_list)
+
+#%%
+# 2
+old_list = [1, 1, 2, 3, 5, 8, 13, 21, 34]
+
+#### convert these four lines to a one-line list comprehension.
+new_list = []
+for num in old_list:
+    if num % 2 == 0:
+        new_list.append(num)
+####
+
+newest_list = [num for num in old_list if num % 2 == 0]
+
+print(new_list)
+print(newest_list)
+
+%##
+
+# BONUS
+old_list = [1,2,3,4,5]
+
+#### convert these four lines to a one-line list comprehension.
+new_list = []
+for item1 in old_list:
+    for item2 in old_list:
+        new_list.append(item1 * item2)
+####
+
+print(new_list)
