dce492ac636e257a93ab586ed60ac73d2c4a8586
detaer <detaer@gmail.com>
2019-03-05 17:42:01 -0800

    adding stuff

---
+++ atbswp3/chapter1/tuples.py
@@ -0,0 +1,18 @@
+""" listTacos = ["shell", "meat", "cheese"]
+print(listTacos[0][2])
+
+print(1 - 88.8)
+print(abs(-88.8))
+
+bacon = 20
+print(bacon + 1)
+print(bacon)
+bacon += 1
+bacon == bacon + 1
+print(bacon) """
+
+plateOFood = 'spam' + 'spamspam'
+print(plateOFood)
+
+sammich = 10.5
+print(str(sammich))
