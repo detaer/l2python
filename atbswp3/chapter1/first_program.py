0a8cff107a1aef5cc77b8f88f6f9a4945d963316
detaer <detaer@gmail.com>
2019-03-05 17:26:57 -0800

    adding stuff

--- atbswp3/chapter1/first_program.py
+++ atbswp3/chapter1/first_program.py
@@ -1,10 +1,20 @@
-# This program says hello and asks for my name.
+This program says hello and asks for my name.
 print('Hello world!')
 print('What is your name?') # ask for their name
 myName = input()
-print('it is good to meet you, ' + myName)
-print('The length of your name is:')
-print(len(myName))
-print('What is your age?') # ask for their age
+myNameLen = str(len(myName))
+print('It is good to meet you, ' + myName)
+print('The length of your name is: ' + myNameLen)
 myAge = input()
+
+print('What is our age?') # ask for their age
 print('You will be ' + str(int(myAge) + 1) + ' in a year.')
+tacos = True
+print(tacos)
+print(type(tacos))
+tacosYum = str(tacos)
+print(tacosYum)
+listTacos = ("shell", "meat", "cheese")
+print(type(listTacos))
+print(listTacos)
+print()
