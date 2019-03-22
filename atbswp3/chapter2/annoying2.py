59f3a7a0ec415e9b8c6afecaaf7f8764a9c51b73
detaer <detaer@gmail.com>
2019-03-21 17:44:14 -0700

    making annoying tacos

---
+++ atbswp3/chapter2/annoying2.py
@@ -0,0 +1,8 @@
+tacos = 0
+while tacos < 3:
+    print ('Please type your name.')
+    name = input()
+    tacos += 1
+    if name == 'your name':
+        print('You did it, now back to the loop! sucker.')
+print('Thank you! jerk')
