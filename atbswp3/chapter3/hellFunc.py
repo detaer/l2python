cdc1a9a685b8b6539719c73d3141e5fcaa9326e8
detaer <detaer@gmail.com>
2019-04-09 18:03:21 -0700

    more things!

---
+++ atbswp3/chapter3/hellFunc.py
@@ -0,0 +1,11 @@
+def hello(name, status):
+    print('Howdy ' + name + '! You are a ' + status + '.')
+    print('Howdy ' + name + '!!! I heard you be ' + status)
+    print('Hello ' + name + '! what what in the ' + status + ' Jeepers!')
+
+hello("Jim", "sucks")
+hello("Fred", "Jumper")
+hello("Taco", "weasel")
+
+hello(status="funty", name="Dianne")
+
---
+++ roundabs.py
@@ -0,0 +1,7 @@
+print(round(5.83483473, 2))
+print(round(6.9393827484, 4))
+print(round(.6, 9))
+
+print(abs(-5))
+print(abs(-10))
+print(abs(-111))
