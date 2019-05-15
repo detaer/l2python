c998cb9f1384dd06778f529a257c38f06c4c42bc
detaer <detaer@gmail.com>
2019-05-14 17:17:54 -0700

    adding more stuff

---
+++ atbswp3/chapter3/saw_you_thinkit.py
@@ -0,0 +1,10 @@
+def spam(divideBy):
+    return 41 / divideBy
+
+try:
+    print(spam(2))
+    print(spam(12))
+    print(spam(0))
+    print(spam(1))
+except ZeroDivisionError:
+    print('Error: Invalid arguement.')
